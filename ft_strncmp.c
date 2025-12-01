14515923dac870ec46c4d46b59e3287814819100
Luciano Petillo <luciano.prnl@gmail.com>
2025-12-01 19:02:03 +0100

    strrchr, strcmp fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: luciano <lupetill@student.42berlin.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 10:07:17 by luciano           #+#    #+#             */
-/*   Updated: 2025/12/01 12:23:10 by luciano          ###   ########.fr       */
+/*   Updated: 2025/12/01 17:42:46 by luciano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,14 +14,22 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	if (n == 0)
+		return (0);
 	i = 0;
-	while (i < n && s1[i] != '\0' && s2[i] != '\0')
+	while (i < n && str1[i] != '\0')
 	{
-		if (s1[i] == s2[i])
+		if (str1[i] == str2[i])
 			i++;
 		else
-			return (s1[i] - s2[i]);
+			return (str1[i] - str2[i]);
 	}
+	if (str1[i] == '\0' || str2[i] == '\0')
+		return (str1[i] - str2[i]);
 	return (0);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,24 +6,25 @@
 /*   By: luciano <lupetill@student.42berlin.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 21:16:48 by luciano           #+#    #+#             */
-/*   Updated: 2025/11/30 21:57:41 by luciano          ###   ########.fr       */
+/*   Updated: 2025/12/01 18:13:17 by luciano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	unsigned int    i;
-	char	*a;
 
-	a = 0;
-	i = 0;
-	while (s[i])
+	i = ft_strlen(s);	
+	while (i > 0)
 	{
 		if ((unsigned char)c == (unsigned char)s[i])
-			a = (char *)(s + i);
-		i++;
+			return (char *)(s + i);
+		i--;
 	}
-	if ((unsigned char)c == '\0' && *a == 0)
+	if ((unsigned char)c == (unsigned char)s[i])
 		return (char *)(s + i);
-	return (a);
+	if ((unsigned char)c == '\0')
+		return (NULL);
+	return (NULL);
 }
