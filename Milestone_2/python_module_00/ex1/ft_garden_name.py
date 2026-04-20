a55d183e18e63a6dec354c8fc4b20c72eca49f89
Luciano Petillo <luciano.prnl@gmail.com>
2026-04-20 20:34:24 +0200

    ex0

---
+++ Milestone_2/python_module_00/ex1/ft_garden_name.py
@@ -0,0 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_garden_name.py                                  :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: luciano <lupetill@student.42berlin.de>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/04/20 20:00:56 by luciano           #+#    #+#              #
+#    Updated: 2026/04/20 20:31:30 by luciano          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def	ft_garden_name():
+	name = input("Enter garden name: \n")
+	print(f"Garden: {name}\nStatus: Growing well!") 
