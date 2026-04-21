9fc742ffcd4012d74bac2d2887994d61faebc5ac
Luciano Petillo <luciano.prnl@gmail.com>
2026-04-21 22:33:55 +0200

    ex2

---
+++ Milestone_2/python_module_00/ex2/ft_plot_area.py
@@ -0,0 +1,18 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_plot_area.py                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: luciano <lupetill@student.42berlin.de>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/04/21 22:10:11 by luciano           #+#    #+#              #
+#    Updated: 2026/04/21 22:33:15 by luciano          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def	ft_plot_area():
+	length = int(input("Enter length: "))
+	width = int(input("Enter width: "))
+	area = length * width
+	print(f"Plot area: {area}")
+	
