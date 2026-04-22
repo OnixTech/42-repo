b0aeda5f847a15bf953fd8bbfeefceb2689c0f34
Luciano Petillo <luciano.prnl@gmail.com>
2026-04-22 09:23:44 +0200

    ex3

---
+++ Milestone_2/python_module_00/ex3/ft_harvest_total.py
@@ -0,0 +1,17 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_harvest_total.py                                :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: luciano <lupetill@student.42berlin.de>     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/04/22 09:18:34 by luciano           #+#    #+#              #
+#    Updated: 2026/04/22 09:23:08 by luciano          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def	ft_harvest_total():
+	first = int(input("harvest: "))
+	second = int(input("harvest: "))
+	third = int(input("harvest: "))
+	print(f"Total harvest: {first + second + third}")
