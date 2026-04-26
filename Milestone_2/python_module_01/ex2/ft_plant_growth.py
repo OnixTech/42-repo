668ffaa510cafaaad84b212a3a9eadc49c739129
Luciano Petillo <luciano.prnl@gmail.com>
2026-04-26 13:52:26 +0200

    ex2

--- Milestone_2/python_module_01/ex2/ft_plant_growth.py
+++ Milestone_2/python_module_01/ex2/ft_plant_growth.py
@@ -3,19 +3,28 @@ class Plant:
         self.name = name
         self.height = height
         self.age = age
+        self.grow_rate = height / age
+    
+    def grow(self) -> None:
+        self.height += self.grow_rate
+
+    def age_day(self) -> None:
+        self.age += 1
 
     def show(self) -> None:
-        i = 1;
-        grow_constant = self.height/self.age
-        print("=== Garden plant Growth ===")
         print(f"{self.name}: {self.height}cm, {self.age} days old")
-        while i <= 7:
-            print(f"=== Day {i} ===")
-            self.height += grow_constant
-            print(f"{self.name}: {self.height}cm, {self.age + i} days old")
-            i += 1
 
 
 if __name__ == "__main__":
     plant = Plant("Tomato", 20, 40)
-    plant.show() 
+    initial_height = plant.height
+    print("=== Garden plant Growth ===")
+    plant.show()
+    for i in range(1, 8):
+        print(f"=== Day {i} ===")
+        plant.grow()
+        plant.age_day()
+        plant.show()
+
+    growth = round(plant.height - initial_height, 1)
+    print(f"Growth this week: {growth}cm")
