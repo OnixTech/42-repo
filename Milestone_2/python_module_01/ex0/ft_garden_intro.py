34ea56e97f54814d9234153b223765250f070c73
Luciano Petillo <luciano.prnl@gmail.com>
2026-04-26 14:10:07 +0200

    ft_intro and ft_data

--- Milestone_2/python_module_01/ex0/ft_garden_intro.py
+++ Milestone_2/python_module_01/ex0/ft_garden_intro.py
@@ -3,8 +3,10 @@ def ft_garden_intro():
     height = 25
     age = 30
     print("=== Welcome to My Garden ===")
-    print(f"Plant: {plant}\nHeight: {height}cm\nAge: {age}days\n")
-    print("=== End of Program  ===")
+    print(f"Plant: {plant}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print("=== End of Program ===")
 
 
 if __name__ == "__main__":
--- Milestone_2/python_module_01/ex1/ft_garden_data.py
+++ Milestone_2/python_module_01/ex1/ft_garden_data.py
@@ -6,3 +6,15 @@ class Plant:
 
     def show(self) -> None:
         print(f"{self.name}: {self.height}cm, {self.age} days old")
+
+
+if __name__ == "__main__":
+    print("=== Garden Plant Registry ===")
+    
+    plant1 = Plant("Tomato", 15, 27)
+    plant2 = Plant("Albaca", 136, 130)
+    plant3 = Plant("Chupapimunino", 10, 76)
+    
+    plant1.show()
+    plant2.show()
+    plant3.show()
