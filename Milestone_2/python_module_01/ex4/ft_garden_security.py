4aa6d8a53fa7ff0e085ed8397b2e231d16a71d1a
Luciano Petillo <luciano.prnl@gmail.com>
2026-04-28 16:24:20 +0200

    ex4

---
+++ Milestone_2/python_module_01/ex4/ft_garden_security.py
@@ -0,0 +1,56 @@
+class Plant:
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self.name = name
+
+        if height < 0:
+            print(f"{name}: Error, height can't be negative")
+            self._height = 0.0
+        else:
+            self._height = height
+        if age < 0:
+            print(f"{name}: Error, age can't be negative")
+            self._age = 0
+        else:
+            self._age = age
+
+    def set_height(self, height: float) -> None:
+        if height < 0:
+            print(f"{self.name}: Error, height can't be negative")
+            print("Height update rejected")
+        else:
+            self._height = height
+            print(f"Height update: {height}cm")
+
+    def set_age(self, age: int) -> None:
+        if age < 0:
+            print(f"{self.name}: Error age can't be negative")
+            print("Age update rejected")
+        else:
+            self._age = age
+            print(f"Age updated: {age} days old")
+
+    def get_height(self) -> float:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._age
+
+    def show(self) -> None:
+            print(f"{self.name}: {self._height}cm, {self._age} days old")
+
+
+if __name__ == "__main__":
+    print("=== Garden Security System ===")
+    
+    plant = Plant("Tomato", 26, 30)
+    print("Plant created:", end = " ")
+    plant.show()
+
+    plant.set_height(35.8)
+    plant.set_age(49)
+
+    plant.set_height(-20.21)
+    plant.set_age(-79)
+
+    print("Current state:", end = " ")
+    plant.show()
