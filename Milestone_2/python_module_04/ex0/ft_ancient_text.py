2a381c892e32e8adaa01817c0a39984f9536aa8d
Luciano Petillo <luciano.prnl@gmail.com>
2026-06-04 18:58:42 +0200

    ex0

---
+++ Milestone_2/python_module_04/ex0/ft_ancient_text.py
@@ -0,0 +1,30 @@
+import sys
+import typing
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print("Usage: ft_ancient_text.py <file>")
+        return
+
+    filename = sys.argv[1]
+
+    print("=== Cyber Archives Recovery ===")
+    print(f"Accesing file '{filename}'")
+
+    try:
+        file: typing.IO[str] = open(filename, "r")
+    except OSError as err:
+        print(f"Error opening file '{filename}': {err}")
+        return
+
+    print("---\n")
+    print(file.read(), end="")
+    print("\n---")
+
+    file.close()
+    print(f"File '{filename}' closed.")
+
+
+if __name__ == "__main__":
+    main()
