285e9ba3bded111d992f469a923690e8340ebe7c
Luciano Petillo <lupetill@c0c7c2.42berlin.de>
2026-06-05 15:03:48 +0200

    flake8 and mypy

---
+++ Milestone_2/python_module_02/ex3/ft_custom_errors.py
@@ -0,0 +1,63 @@
+class GardenError(Exception):
+    def __init__(self: "GardenError",
+                 message: str = "Unknown garden error") -> None:
+        super().__init__(message)
+
+
+class PlantError(GardenError):
+    def __init__(self: "PlantError",
+                 message: str = "Unknown plant error") -> None:
+        super().__init__(message)
+
+
+class WaterError(GardenError):
+    def __init__(self: "WaterError",
+                 message: str = "Unknown water error") -> None:
+        super().__init__(message)
+
+
+def test_plant_error() -> None:
+    raise PlantError("The plant is drying out!")
+
+
+def test_water_error(water_level: int) -> None:
+    if water_level < 4:
+        raise WaterError("The tank is getting empty!")
+
+
+def main() -> None:
+    print("=== Custom Garden Errors Demo ===")
+
+    try:
+        print("\nTesting PlantError...")
+        test_plant_error()
+        print("\nTesting WaterError...")
+        test_water_error(3)
+
+    except PlantError as er:
+        print(f"Caught PlantError: {er}")
+
+    try:
+        test_water_error(3)
+
+    except WaterError as er:
+        print(f"Caught WaterError: {er}")
+
+    print("\nTesting catching all garden errors...")
+    try:
+        test_plant_error()
+
+    except GardenError as er:
+        print(f"Caught GardenError: {er}")
+
+    try:
+        test_water_error(2)
+
+    except GardenError as er:
+        print(f"Caught GardenError: {er}")
+
+    print("\nAll custom error types work correctly!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Milestone_2/python_module_02/ex4/ft_finally_block.py
@@ -0,0 +1,47 @@
+class PlantError(Exception):
+    def __init__(self: "PlantError",
+                 message: str = "Unknown plant error") -> None:
+        super().__init__(message)
+
+
+def water_plant(name: str) -> None:
+    if name != name.capitalize():
+        raise PlantError(f"Invalid plant name to water: '{name}'")
+
+    print(f"Watering {name}: [OK]")
+
+
+def main() -> None:
+    print("=== Garden Watering System ===")
+
+    try:
+        print("\nTesting VALID names")
+        water_plant("Carrot")
+        water_plant("Lettuce")
+        water_plant("Banana")
+
+    except PlantError as er:
+        print(f"Caught PlantError: {er}")
+        print("... ending tests and returning to main")
+        return
+
+    finally:
+        print("Closing watering system")
+
+    try:
+        print("\nTesting one INVALID (lettuce) name")
+        water_plant("Carrot")
+        water_plant("lettuce")
+        water_plant("Banana")
+
+    except PlantError as er:
+        print(f"Caught PlantError: {er}")
+        print("... ending tests and returning to main")
+        return
+
+    finally:
+        print("Closing watering system")
+
+
+if __name__ == "__main__":
+    main()
